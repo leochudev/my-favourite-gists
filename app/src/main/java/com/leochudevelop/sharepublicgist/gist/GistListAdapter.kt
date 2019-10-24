bea15cd460b3e529676359be1e643f4284d72728
leochudev <leochudevelop@gmail.com>
2019-10-24 17:18:49 +0900

    Add click event to gist list item

--- app/src/main/java/com/leochudevelop/sharepublicgist/gist/GistListAdapter.kt
+++ app/src/main/java/com/leochudevelop/sharepublicgist/gist/GistListAdapter.kt
@@ -1,32 +1,49 @@
 package com.leochudevelop.sharepublicgist.gist
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.leochudevelop.sharepublicgist.R
+import kotlinx.android.synthetic.main.gist_item_view.view.*
 
 /**
  * A recycler view adapter to display the gist information.
  */
 class GistListAdapter : ListAdapter<Gist, GistListAdapter.MyViewHolder>(GistDiffCallback()) {
 
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class MyViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+
+        private val textView: TextView = root.id_text_view
+        private var actionId: String? = null
+
+        init {
+            root.setOnClickListener {
+                Log.i(GistListAdapter::class.java.simpleName, actionId ?: "")
+            }
+        }
+
+        fun bind(id: String) {
+            textView.text = id
+            actionId = id
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.gist_item_view, parent, false) as TextView
+        val gistItemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.gist_item_view, parent, false)
 
         // TODO: set the view's size, margins, paddings and layout parameters
 
-        return MyViewHolder(textView)
+        return MyViewHolder(gistItemView)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = getItem(position).id
-    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) =
+        holder.bind(getItem(position).id)
 }
 
 private class GistDiffCallback : DiffUtil.ItemCallback<Gist>() {
